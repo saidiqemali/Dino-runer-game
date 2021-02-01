66672e8896032e0bb88c1dcc952fa3e822f97608
Saidi Qemali <78086708+saidiqemali@users.noreply.github.com>
2021-02-01 17:44:18 +0100

    Add files via upload

---
+++ Form 1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace degkjsokg
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 14;
+        int score = 0;
+        int obstacleSpeed = 10;  
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            GameReset();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void obstacle2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainGameTimerEvnet(object sender, EventArgs e)
+        {
+            Dino.Top += jumpSpeed;
+
+            textscore.Text = "Score: " + score;
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -14;
+                force = 1;
+            }
+            else
+            {
+                jumpSpeed = 14;
+            }
+
+            if (Dino.Top > 366 && jumping == false)
+            {
+                force = 14;
+                Dino.Top = 367;
+                jumpSpeed = 0;
+            }
+
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstace")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++; 
+                    }
+
+                    if (Dino.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameTimer.Stop();
+
+                        textscore.Text += " Press R to restart game";
+                        isGameOver = true;
+
+                    }
+                }
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            textscore.Text = "Score: " + score;
+            score = 0;
+            obstacleSpeed = 10;
+            obstacleSpeed = 10;
+            isGameOver = false;
+            Dino.Top = 367;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+
+                }
+
+                GameTimer.Start();
+
+
+            }
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace degkjsokg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Dino = new System.Windows.Forms.PictureBox();
+            this.obstacle = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.textscore = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Dino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dino
+            // 
+            this.Dino.Image = ((System.Drawing.Image)(resources.GetObject("Dino.Image")));
+            this.Dino.Location = new System.Drawing.Point(52, 367);
+            this.Dino.Name = "Dino";
+            this.Dino.Size = new System.Drawing.Size(76, 74);
+            this.Dino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Dino.TabIndex = 0;
+            this.Dino.TabStop = false;
+            this.Dino.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // obstacle
+            // 
+            this.obstacle.Image = ((System.Drawing.Image)(resources.GetObject("obstacle.Image")));
+            this.obstacle.Location = new System.Drawing.Point(305, 394);
+            this.obstacle.Name = "obstacle";
+            this.obstacle.Size = new System.Drawing.Size(67, 47);
+            this.obstacle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.obstacle.TabIndex = 1;
+            this.obstacle.TabStop = false;
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = ((System.Drawing.Image)(resources.GetObject("obstacle2.Image")));
+            this.obstacle2.Location = new System.Drawing.Point(614, 388);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(59, 53);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.obstacle2.TabIndex = 2;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Click += new System.EventHandler(this.obstacle2_Click);
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.Black;
+            this.floor.Location = new System.Drawing.Point(-1, 438);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(802, 12);
+            this.floor.TabIndex = 3;
+            this.floor.TabStop = false;
+            // 
+            // textscore
+            // 
+            this.textscore.AutoSize = true;
+            this.textscore.Location = new System.Drawing.Point(0, 0);
+            this.textscore.Name = "textscore";
+            this.textscore.Size = new System.Drawing.Size(50, 20);
+            this.textscore.TabIndex = 4;
+            this.textscore.Text = "label1";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvnet);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 444);
+            this.Controls.Add(this.textscore);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle);
+            this.Controls.Add(this.Dino);
+            this.Name = "Form1";
+            this.Text = "Score: 0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Dino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Dino;
+        private System.Windows.Forms.PictureBox obstacle;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.Label textscore;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
